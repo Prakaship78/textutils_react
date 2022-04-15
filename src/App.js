6185deeaa02973f5e6cd3174adb27bf8a522b3da
prakaship78 <prakash.singh@american-technology.net>
2022-04-15 13:05:40 +0530

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
 import "./App.css";
+import React, { useState } from "react";
+
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode is enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+      // setInterval(() => {
+      //   document.title = "TextUtils is amaizing";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils now";
+      // }, 1500);
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode is enabled", "success");
+      document.title = "TextUtils - Light Mode";
+    }
+  };
   return (
-    <>
-      <Navbar title="Text Utils" aboutText="About TextUtils" />
-      <TextForm heading="Enter the text below" />
-    </>
+    <Router>
+      <>
+        <Navbar
+          title="Text Utils"
+          aboutText="About TextUtils"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert}></Alert>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/">
+            <TextForm
+              showAlert={showAlert}
+              heading="Enter the text below"
+              mode={mode}
+            />
+          </Route>
+        </Switch>
+      </>
+    </Router>
   );
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,29 +3,65 @@ import React, { useState } from "react";
 export default function TextForm(props) {
   const changeToUpCase = () => {
     setText(text.toUpperCase());
+    props.showAlert("Converted to UpperCase", "success");
+  };
+  const changeToLoCase = () => {
+    setText(text.toLowerCase());
+    props.showAlert("Converted to LowerCase", "success");
   };
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
-  const [text, setText] = useState("Enter the text");
+  const [text, setText] = useState("");
 
   return (
-    <div className="container my-3">
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <label htmlFor="myBox" className="form-label"></label>
-        <textarea
-          className="form-control"
-          //   placeholder="Enter Your text here"
-          id="myBox"
-          rows="5"
-          value={text}
-          onChange={handleOnChange}
-        ></textarea>
+    <>
+      <div className="container my-3">
+        <h1
+          style={{
+            color: props.mode === "light" ? "black" : "white",
+          }}
+        >
+          {props.heading}
+        </h1>
+        <div className="mb-3">
+          <label htmlFor="myBox" className="form-label"></label>
+          <textarea
+            className="form-control"
+            placeholder="Enter Your text here"
+            id="myBox"
+            rows="5"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "black",
+              color: props.mode === "light" ? "black" : "white",
+            }}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-3" onClick={changeToUpCase}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={changeToLoCase}>
+          Convert to LowerCase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={changeToUpCase}>
-        Convert to UpperCase
-      </button>
-    </div>
+      <div className="container my3">
+        <h1
+          style={{
+            color: props.mode === "light" ? "black" : "white",
+          }}
+        >
+          Text Summary
+        </h1>
+        <h4
+          style={{
+            color: props.mode === "light" ? "black" : "white",
+          }}
+        >
+          {text.split(" ").length} words and {text.length} characters.
+        </h4>
+      </div>
+    </>
   );
 }
